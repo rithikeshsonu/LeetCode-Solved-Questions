651027b6f87648058ae04620a57b400b8ed19eaa
rithikeshsonu <36332866+rithikeshsonu@users.noreply.github.com>
2022-11-22 01:24:26 +0530

    Time: 5 ms (14.54%), Space: 5.9 MB (68.71%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.cpp
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.cpp
@@ -6,18 +6,9 @@
 			while(low<=high)
 			{
 				mid= (low+high)/2;
-				if(guess(mid)==1)           //guess API
-				{
-					low=mid+1;
-				}
-				else if(guess(mid)==-1)         //guess API
-				{
-					high=mid-1;
-				}
-				else
-				{
-					return mid;
-				}
+				if(guess(mid)==1) low=mid+1; //guess API
+				else if(guess(mid)==-1)	high=mid-1;
+				else return mid;
 			}
 			return low;
 		}
